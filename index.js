58fa02e1d950a0daa70d65c13fa9c9938f28c643
Max Hsu <maxhsu@botbonnie.com>
2019-04-29 12:33:46 -0700

    Set access control allow headers header in middleware

--- index.js
+++ index.js
@@ -143,6 +143,7 @@ const requests = [
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next()
 })
 
